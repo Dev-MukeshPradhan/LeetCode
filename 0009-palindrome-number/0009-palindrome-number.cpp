949415cca150601edb931652447a3748ba8c12f6
Dev-MukeshPradhan <2024ugcm023@nitjsr.ac.in>
2026-06-27 10:17:26 +0530

    Time: 3 ms (45.17%), Space: 8.6 MB (65.6%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int num=x;
+        int rem;
+        int ans=0;
+        if(x<0){
+            return 0;
+        }
+
+        while(num!=0){
+            rem=num%10;
+            num=num/10;
+            if(ans>(INT_MAX)/10){
+                return 0;
+            }
+            ans=ans*10+rem;
+        }
+
+        if(ans==x){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+        
+    }
+};
