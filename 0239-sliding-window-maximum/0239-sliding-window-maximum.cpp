308126493f9cb645bd710a5d63fc4d8b3794d862
Dev-MukeshPradhan <2024ugcm023@nitjsr.ac.in>
2026-08-27 12:20:08 +0530

    Time: 27 ms (42.53%), Space: 139.3 MB (26.45%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n=nums.size();
-        int ngi[n];
-        stack<int> st;
-        ngi[n-1]=n;
-        st.push(n-1);
-        for(int i=n-2; i>=0; i--){
-            while(st.size()>0 && nums[st.top()]<=nums[i]){
-                st.pop();
-            }
-            if(st.size()==0) ngi[i]=n;
-            else ngi[i]=st.top();
-            st.push(i);
-        }
+        int n = nums.size();
         vector<int> ans;
-        int j=0;
-        for(int i=0; i<n-k+1; i++){
-            if(j<i) j=i;
-            int max=nums[j];
-            while(j<i+k){
-                max=nums[j];
-                if(ngi[j]>=i+k) break;
-                j=ngi[j];
+        deque<int> dq;
+
+        for (int i = 0; i < n; i++) {
+
+            // Remove elements outside the window
+            while (!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
             }
-            ans.push_back(max);
+
+            // Remove smaller elements from the back
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+
+            // Add current index
+            dq.push_back(i);
+
+            // Start storing answers when window size becomes k
+            if (i >= k - 1) {
+                ans.push_back(nums[dq.front()]);
+            }
         }
+
         return ans;
-        
     }
 };
