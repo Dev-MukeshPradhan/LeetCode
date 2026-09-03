a6e3ee4794dcb21c1ef18f7352138e728df05285
Dev-MukeshPradhan <2024ugcm023@nitjsr.ac.in>
2026-09-03 12:07:07 +0530

    Time: 7 ms (3.45%), Space: 16.8 MB (5.05%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-
-    void solve(int idx, vector<int>& nums,
-               vector<int>& temp,
-               vector<vector<int>>& ans) {
-
-        // Every temp is a valid subset
-        ans.push_back(temp);
-
-        // Try every element from idx onwards
-        for(int i = idx; i < nums.size(); i++) {
-
-            // Choose
-            temp.push_back(nums[i]);
-
-            // Move to next element
-            solve(i + 1, nums, temp, ans);
-
-            // Undo choice
-            temp.pop_back();
+    void helper(vector<int> nums, vector<int>ans, vector<vector<int>>& finalAns, int idx){
+        if(idx==nums.size()){
+            finalAns.push_back(ans);
+            return;
         }
+        helper(nums,ans,finalAns,idx+1);
+        ans.push_back(nums[idx]);
+        helper(nums,ans,finalAns,idx+1);
+        
+
     }
-
     vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>ans;
+        vector<vector<int>>finalAns;
+        helper(nums,ans,finalAns,0);
+        return finalAns;
 
-        vector<vector<int>> ans;
-        vector<int> temp;
-
-        solve(0, nums, temp, ans);
-
-        return ans;
+        
     }
 };
