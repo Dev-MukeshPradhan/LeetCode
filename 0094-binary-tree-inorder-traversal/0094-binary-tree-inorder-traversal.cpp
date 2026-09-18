6d5476b25e01b1e054323b4c6ee50bd31b901c69
Dev-MukeshPradhan <2024ugcm023@nitjsr.ac.in>
2026-09-18 11:37:46 +0530

    Time: 0 ms (100%), Space: 10.9 MB (34.82%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,40 +1,53 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    // void helper(TreeNode* root, vector<int> &ans){
-    //     if(root==NULL) return;
-    //     helper(root->left,ans);
-    //     ans.push_back(root->val);
-    //     helper(root->right,ans);
-
-    // }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        stack<TreeNode*> st;
-        TreeNode* it_node = root;
-        while(st.size()>0 || it_node!=NULL){
-            if(it_node){
-                st.push(it_node);
-                it_node=it_node->left;
-            }else{
-                TreeNode* temp = st.top();
-                st.pop();
-                ans.push_back(temp->val);
-                it_node=temp->right;
+        vector<int> ans;
+
+        TreeNode* curr = root;
+
+        while (curr != NULL) {
+
+            if (curr->left != NULL) {
+
+                // Find inorder predecessor
+                TreeNode* pred = curr->left;
+
+                while (pred->right != NULL && pred->right != curr) {
+                    pred = pred->right;
+                }
+
+                if (pred->right == NULL) {
+
+                    // Create temporary link
+                    pred->right = curr;
+
+                    // Move to left subtree
+                    curr = curr->left;
+
+                } 
+                else {
+
+                    // Remove temporary link
+                    pred->right = NULL;
+
+                    // Visit current node
+                    ans.push_back(curr->val);
+
+                    // Move to right subtree
+                    curr = curr->right;
+                }
 
             } 
+            else {
+
+                // No left subtree → visit immediately
+                ans.push_back(curr->val);
+
+                // Move right
+                curr = curr->right;
+            }
         }
+
         return ans;
-        
     }
 };
