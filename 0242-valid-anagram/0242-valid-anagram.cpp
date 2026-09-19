c890a50b2278cc754a98ee9441a614861ec2c80b
Dev-MukeshPradhan <2024ugcm023@nitjsr.ac.in>
2026-09-19 14:00:20 +0530

    Time: 4 ms (33.72%), Space: 9.9 MB (25.37%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,12 +2,25 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false;
+        unordered_map<char,int> m1; // s
+        unordered_map<char,int> m2; // t
+        for(int i=0; i<s.size(); i++){
+            m1[s[i]]++;
+        } 
+        for(int i=0; i<t.size(); i++){
+            m2[t[i]]++;
+        }
+        for(auto x : m1){
+            char ch1 = x.first;
+            int freq1 = x.second;
+            if(m2.find(ch1)!=m2.end()){
+                int freq2 = m2[ch1];
+                if(freq1!=freq2) return false;
+            }else{
+                return false;
+            }
 
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-
-        if(s==t) return true;
-        else return false;
-        
+        }
+        return true;
     }
 };
